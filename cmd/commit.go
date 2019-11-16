583308fb213a4762b7baafcef7fca6da8afd2843
Patrick McFadden <pemcfadden@gmail.com>
2019-11-16 16:55:01 -0700

    [NONE][Patrick] Fixing help message

--- cmd/commit.go
+++ cmd/commit.go
@@ -33,13 +33,10 @@ var dryRun bool
 // commitCmd represents the commit command
 var commitCmd = &cobra.Command{
 	Use:   "commit",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Make a commit",
+	Long: `Create a commit using a format defined in your config file
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	It will ask for your current story, pair and message`,
 	Run: func(cmd *cobra.Command, args []string) {
 		directory, err := os.Getwd()
 		_CheckIfError(err)
