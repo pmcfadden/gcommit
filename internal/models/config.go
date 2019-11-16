fb11e42f9bba28e1734e0c3f7cdf29a56046dca9
Patrick McFadden <pemcfadden@gmail.com>
2019-11-16 09:30:22 -0700

    [NONE][Patrick] DRYing up taking user input

--- internal/models/config.go
+++ internal/models/config.go
@@ -18,24 +18,26 @@ type Config struct {
 }
 
 func ReadAndSetConfig(stdin io.Reader) Config {
-	reader := bufio.NewReader(stdin)
 	format := viper.GetString("format")
 	story := viper.GetString("story")
 	pair := viper.GetString("pair")
 	name := viper.GetString("author.name")
 	email := viper.GetString("author.email")
 	fmt.Printf("Enter Story [%s]: ", story)
-	maybeStory, _ := reader.ReadString('\n')
-	if maybeStory != "\n" {
-		story = strings.TrimSuffix(maybeStory, "\n")
-	}
+	story = readInputWithDefault(stdin, story)
 	fmt.Printf("Enter Pair [%s]: ", pair)
-	maybePair, _ := reader.ReadString('\n')
-	if maybePair != "\n" {
-		pair = strings.TrimSuffix(maybePair, "\n")
-	}
+	pair = readInputWithDefault(stdin, pair)
 	viper.Set("story", story)
 	viper.Set("pair", pair)
 	viper.WriteConfig()
 	return Config{Format: format, Story: story, Pair: pair, Name: name, Email: email}
+}
+
+func readInputWithDefault(stdin io.Reader, original string) string {
+	reader := bufio.NewReader(stdin)
+	maybeChange, _ := reader.ReadString('\n')
+	if maybeChange != "\n" {
+		original = strings.TrimSuffix(maybeChange, "\n")
+	}
+	return original
 }
